7265fc9d91936f451614e5f11e1233b493fe1f16
Denis Salnikov <denissalnikoff@mail.ru>
2023-09-15 16:08:45 +0400

    for push

--- index.js
+++ index.js
@@ -3,45 +3,44 @@ import { resolve, extname } from 'node:path';
 import { cwd } from 'node:process';
 import getParser from './src/parsers.js';
 
-const genAddKey = (key) => `+ ${key}`;
-const genRemoveKey = (key) => `- ${key}`;
-const genIntactKey = (key) => `  ${key}`;
-
-const genObjDiff = (obj1, obj2) => {
+const genAST = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
 
-  let diffObj = keys1.reduce((acc, key) => {
-    // сразу определяем ключ возможного изменения
-    const isIntactKey = (obj1[key] instanceof Object && obj2[key] instanceof Object)
+  let ast = keys1.reduce((acc, key) => {
+    // определяем статус свойства. Для первого файла может быть либо 'intact' либо 'removed'.
+    const isIntact = (obj1[key] instanceof Object && obj2[key] instanceof Object)
       || (obj1[key] === obj2[key]);
-    const diffKey = isIntactKey ? genIntactKey(key) : genRemoveKey(key);
-    // рекурсия, если свойство является объектом
-    if (obj1[key] instanceof Object) {
-      acc[diffKey] = genObjDiff(obj1[key], (obj2[key] instanceof Object) ? obj2[key] : obj1[key]);
-      return acc;
-    }
-    // обычная обработка, если свойство является значением
-    acc[diffKey] = obj1[key];
-    return acc;
-  }, {});
-
-  diffObj = keys2.reduce((acc, key) => {
-    // сразу определяем ключ возможного изменения
-    const diffKey = genAddKey(key);
-    // рекурсия, если свойство является объектом
-    if (obj2[key] instanceof Object) {
-      if (!(obj1[key] instanceof Object)) { // если этот случай не обработан предыдущем цикле
-        acc[diffKey] = genObjDiff(obj2[key], obj2[key]);
+    const state = isIntact ? 'intact' : 'removed';
+    const getValue = () => {
+      if (obj1[key] instanceof Object) {
+        return genAST(obj1[key], (obj2[key] instanceof Object) ? obj2[key] : obj1[key]);
       }
-      return acc;
-    }
-    // обычная обработка, если свойство является значением
-    if (obj2[key] !== obj1[key]) acc[diffKey] = obj2[key];
+      return obj1[key];
+    };
+    const value = getValue();
+    acc.push({ key, value, state });
     return acc;
-  }, diffObj);
+  }, []);
 
-  return diffObj;
+  ast = keys2.reduce((acc, key) => {
+    // проверяем необходимость добавления элемента
+    const astElement = ast.find((el) => el.key === key);
+    if (astElement && astElement.state === 'intact') return acc;
+    // определяем статус свойства. Для второго файла может быть только 'added'.
+    const state = 'added';
+    const getValue = () => {
+      if (obj2[key] instanceof Object) {
+        return genAST(obj2[key], obj2[key]);
+      }
+      return obj2[key];
+    };
+    const value = getValue();
+    acc.push({ key, value, state });
+    return acc;
+  }, ast);
+
+  return ast;
 };
 
 const gendiff = (filepath1, filepath2, formatter) => {
@@ -53,7 +52,7 @@ const gendiff = (filepath1, filepath2, formatter) => {
   }
   const obj1 = parser(file1);
   const obj2 = parser(file2);
-  const diffObj = genObjDiff(obj1, obj2);
+  const diffObj = genAST(obj1, obj2);
   return formatter(diffObj);
 };
 
