7f714332f320b13d4279c7a0322f7f82f9e128c7
Denis Salnikov <denissalnikoff@mail.ru>
2023-09-17 14:12:02 +0400

    fix: src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,16 +2,13 @@ import genStylish from './stylish.js';
 import genPlain from './plain.js';
 import genJSON from './json.js';
 
-export default (f) => {
-  const format = f.toLowerCase();
+export default (format) => {
   switch (format) {
-    case 'stylish':
-      return genStylish;
     case 'plain':
       return genPlain;
     case 'json':
       return genJSON;
     default:
-      return undefined;
+      return genStylish;
   }
 };
